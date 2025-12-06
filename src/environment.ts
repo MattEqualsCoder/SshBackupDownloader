970ac659d4da427030ada975c17d99222c26f49a
MattEqualsCoder <mattequalscoder@gmail.com>
2025-12-05 22:43:39 -0500

    Adding comments

--- src/environment.ts
+++ src/environment.ts
@@ -6,10 +6,10 @@ const RemoteDirectory = process.env.REMOTE_DIRECTORY;
 const LocalDirectory = process.env.LOCAL_DIRECTORY;
 const SshHost = process.env.SSH_HOST;
 const SshPort = process.env.SSH_PORT;
-const SshUser = process.env.SSH_USER;
-const SshPassword = process.env.SSH_PASSWORD;
-const SshKeyPath = process.env.SSH_KEY_PATH;
-const SshKeySecret = process.env.SSH_KEY_SECRET;
+const SshUser = process.env.SSH_USER; // User to connect to
+const SshPassword = process.env.SSH_PASSWORD; // Password for the user
+const SshKeyPath = process.env.SSH_KEY_PATH; // Path to the key file for connecting to ssh
+const SshKeySecret = process.env.SSH_KEY_SECRET; // Secret for accessing the key file
 const MaxBackups = parseInt(process.env.MAX_BACKUPS ?? "30") // Only contain 30 days of backups
 
 export {
